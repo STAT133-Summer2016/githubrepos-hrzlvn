0400d0299a2b4d93897c019a9a5384780dcfe0b0
hrzlvn <hrzlvn@gmail.com>
2016-07-13 22:41:57 -0700

    finished lab20 and hw5

---
+++ labs5/lab20/ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+shinyUI(fluidPage(
+	titlePanel("Occupancy Rates in Berkeley"),
+	
+	sidebarLayout(
+		sidebarPanel(
+			helpText("Upload an XML file with the Census Data to get started"),
+			fileInput("file", label = "File input"),
+			selectInput("neighbor",
+				label = "Choose the neighborhood to inspect",
+				choices = list("North Berkeley",
+								"Rockridge",
+								"Downtown Berkeley",
+								"Berkeley Hills"),
+				selected = "North Berkeley"), 
+			
+			sliderInput("Occupancy",
+				label = "Number of People in Household",
+				min = 0,
+				max = 40,
+				value = c(0, 40))
+			),
+				
+			mainPanel()
+	)
+))
